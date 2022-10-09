381f5431db7d8cefa2850f1ae3cac8e903745985
DevelSajjad <sinigdho01@gmail.com>
2022-10-09 08:50:58 +0600

    hospital project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,15 +11,17 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        <link rel="stylesheet" href=" {{asset('css/app.css')}} ">
         @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="{{asset('js/app.js')}}"></script>
+        
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h2>This is admin page</h2>
+ </x-app-layout>
